c8a59db60fb059a4340b10657a86aba334dcd4a4
VolkanKabay <124468930+VolkanKabay@users.noreply.github.com>
2025-03-13 23:54:57 +0100

    Revert "Update backend.ts"
    This reverts commit de80314345caa4008f002182b2eb35f63d52eb71.

--- src/api/backend.ts
+++ src/api/backend.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const backend = axios.create({
-  baseURL: "https://hardware-information-react.vercel.app/api",
+  baseURL: "localhost:3000/api",
 });
 
 export const get = async (url: string) => {
